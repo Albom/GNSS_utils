b1546456f98dd9bec4adde40ed597704da117c75
Albom <albom85@yandex.ru>
2017-12-07 10:55:08 +0200

    GPS week calculation

---
+++ gnss/date.py
@@ -0,0 +1,14 @@
+'''
+(c) 2017 Oleksandr Bogomaz
+
+Date utils.
+'''
+
+
+import datetime
+
+def week(year, month, day):
+    d = datetime.date(year, month, day)
+    e = datetime.date(1980, 1, 13)
+    diff = d-e
+    return diff.days//7+1
---
+++ test_date.py
@@ -0,0 +1,9 @@
+'''
+(c) 2017 Oleksandr Bogomaz
+
+Tests of date utils.
+'''
+
+import gnss.date as date
+
+print(date.week(2017, 12, 7))
