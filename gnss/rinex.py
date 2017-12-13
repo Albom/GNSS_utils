ac3fc94298fee4a5d4fa2b87ffbd720a405a6e0b
Albom <albom85@yandex.ru>
2017-12-13 11:36:40 +0200

    Export date and number of satellites

--- gnss/rinex.py
+++ gnss/rinex.py
@@ -40,18 +40,19 @@ def read_obs(filename):
                     obs = line[6 + 6 * i:6 + 6 * (i + 1)].strip()
                     header.add_types_of_obs(obs)
 
+    data = []
     while n < len(lines):
         line = lines[n]
-        num_of_sat = int(line[30:32])
         year = int(line[1:3])
         year += 2000 if year < 80 else 1900
         month = int(line[4:6])
         day = int(line[7:9])
         hh = int(line[10:12])
-        mm = int(line[13:14])
+        mm = int(line[13:15])
         ss = float(line[15:26])
         date = datetime(year, month, day, hh, mm, int(ss), int((ss % 1) * 1e6))
-        print(date.isoformat(), num_of_sat)
+        num_of_sat = int(line[30:32])
+        data.append({'date': date, 'num': num_of_sat})
         n += num_of_sat * 2 + 1 + (1 if num_of_sat > 12 else 0)
 
-    return {'header': header}
+    return {'header': header, 'data': data}
