29793b9895f4bfbbd506a80042119cc266a6a3d4
Albom <albom85@yandex.ru>
2018-02-01 15:14:48 +0200

    Print G01 data

--- test_rinex.py
+++ test_rinex.py
@@ -16,3 +16,17 @@ print(header.get_num_of_obs(), header.get_types_of_obs())
 data = r['data']
 print(len(data))
 print(data[0])
+
+sat = set()
+for d in data:
+    for s in d['sat']:
+        sat.add(s)
+sat = list(sat)
+sat.sort()
+print(sat)
+
+for d in data:
+    params = d['param']
+    for k, p in params.items():
+        if k == "G01":
+            print(p[0], p[4], sep='\t')
