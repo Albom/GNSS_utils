dc8d8082fe95718136f5d7b40c5eb53561f613fa
Albom <albom85@yandex.ru>
2018-03-27 14:21:05 +0300

    Errors fixed

--- gnss/coord.py
+++ gnss/coord.py
@@ -10,21 +10,24 @@ Utilities for coordinates converting.
 import math
 
 
-class ELLIPSE:
+class WGS_84:
     A = 6378137.0
     B = 6356752.31414
-    E2 = 0.00669438002290
+    E2 = (A ** 2 - B ** 2) / A ** 2
+
+
+ELLIPSE = WGS_84
 
 
 def xyz(**kwargs):
-    rad = math.pi/180.0
-    lat = kwargs['lat']*rad
-    long = kwargs['long']*rad
+    rad = math.pi / 180.0
+    lat = kwargs['lat'] * rad
+    long = kwargs['long'] * rad
     height = kwargs['height']
-    n = ELLIPSE.A / math.sqrt(1 - ELLIPSE.E2*math.sin(lat)**2)
+    n = ELLIPSE.A / math.sqrt(1.0 - ELLIPSE.E2 * math.sin(lat) ** 2.0)
     x = (n + height) * math.cos(lat) * math.cos(long)
     y = (n + height) * math.cos(lat) * math.sin(long)
-    z = ((1 - ELLIPSE.E2)*n + height) * math.sin(lat)
+    z = ((1.0 - ELLIPSE.E2) * n + height) * math.sin(lat)
     return {'x': x, 'y': y, 'z': z}
 
 
@@ -32,23 +35,23 @@ def geo(**kwargs):
     x = kwargs['x']
     y = kwargs['y']
     z = kwargs['z']
-    eps = 1e-4
-    deg = 180.0/math.pi
+    eps = 1.0e-15
     if (math.fabs(x) < eps) & (math.fabs(y) < eps):
-        lat = math.copysign(math.pi/2, z)
-        long = 0
-        height = math.fabs(z)-ELLIPSE.B
+        lat = math.copysign(math.pi / 2, z)
+        long = 0.0
+        height = math.fabs(z) - ELLIPSE.B
     else:
         p = math.hypot(x, y)
-        long = 2*math.atan2(y, x+p)
-        lat = math.atan(z/(p*(1-ELLIPSE.E2)))
-        height = 0
+        long = 2.0 * math.atan2(y, x + p)
+        lat = math.atan(z / (p * (1.0 - ELLIPSE.E2)))
+        height = 0.0
         while True:
             prev = height
-            n = ELLIPSE.A / math.sqrt(1 - ELLIPSE.E2*math.sin(lat)**2)
-            height = p/math.cos(lat) - n
-            lat = math.atan(z/(p*(1-ELLIPSE.E2*n/(n-height))))
-            if math.fabs(height-prev) < eps:
+            n = ELLIPSE.A / math.sqrt(1 - ELLIPSE.E2 * math.sin(lat) ** 2)
+            height = p / math.cos(lat) - n
+            lat = math.atan(z / (p * (1 - ELLIPSE.E2 * n / (n + height))))
+            if math.fabs(height - prev) < eps:
                 break
 
-        return {'lat': lat*deg, 'long': long*deg, 'height': height}
+        deg = 180.0 / math.pi
+        return {'lat': lat * deg, 'long': long * deg, 'height': height}
--- test_coord.py
+++ test_coord.py
@@ -8,7 +8,10 @@ Tests of coordinates converting.
 import gnss.coord as coord
 
 if __name__ == '__main__':
-    print(coord.geo(x=3411557.3382, y=2348463.8352, z=4834396.6426))
-    print(coord.xyz(lat=67.251508, long=26.232056, height=299.7))
 
-    print(coord.geo(**coord.xyz(lat=67.251508, long=26.232056, height=299.7)))
+    d1 = {'x': 3411557.3382, 'y': 2348463.8352, 'z': 4834396.6426}
+    d2 = d1
+    for i in range(0, 1000):
+        g1 = coord.geo(**d2)
+        d2 = coord.xyz(**g1)
+    print(d1['x']-d2['x'], d1['y']-d2['y'], d1['z']-d2['z'], sep='\n')
